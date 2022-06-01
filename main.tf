e4c1e9db9bcde041109591849954497c8762ec1a
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-01 20:34:50 +0530

    configured internet gateway

--- main.tf
+++ main.tf
@@ -1,20 +1,28 @@
 resource "aws_vpc" "mtc_vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block           = "10.0.0.0/16"
     enable_dns_hostnames = true
-    enable_dns_support = true
-
+    enable_dns_support   = true
+    
     tags = {
         Name = "dev"
     }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-    vpc_id     = aws_vpc.mtc_vpc.id
-    cidr_block = "10.0.1.0/24"
+    vpc_id                  = aws_vpc.mtc_vpc.id
+    cidr_block              = "10.0.1.0/24"
     map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
-
+    availability_zone       = "us-east-1a"
+    
     tags = {
         Name = "dev-public"
+    }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+    vpc_id = aws_vpc.mtc_vpc.id
+    
+    tags = {
+        Name = "dev-igw"
     }
 }
