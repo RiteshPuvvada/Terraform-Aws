1133c7846a025546572346ab90e506fbee63abe6
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-01 20:44:01 +0530

    added and configured route table

--- main.tf
+++ main.tf
@@ -26,3 +26,17 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
         Name = "dev-igw"
     }
 }
+
+resource "aws_route_table" "mtc_public_rt" {
+    vpc_id = aws_vpc.mtc_vpc.id
+
+    tags = {
+        Name = "dev_public_rt"
+    }
+}
+
+resource "aws_route" "default_route" {
+    route_table_id = aws_route_table.mtc_public_rt.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mtc_internet_gateway.id
+}
