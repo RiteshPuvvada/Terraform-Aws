58c6845bddf4f9c7aaa822ac102e8feb2355904f
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-02 15:15:52 +0530

    Configured security groups

--- main.tf
+++ main.tf
@@ -45,3 +45,23 @@ resource "aws_route_table_association" "mtc_public_assoc" {
     subnet_id      = aws_subnet.mtc_public_subnet.id
     route_table_id = aws_route_table.mtc_public_rt.id
 }
+
+resource "aws_security_group" "mtc_sg" {
+    name        = "dev-sg"
+    description = "Dev security group"
+    vpc_id      = aws_vpc.mtc_vpc.id
+    
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+        
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
