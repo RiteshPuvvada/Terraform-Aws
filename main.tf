58f8a5225e265c9687408481d82bd5eb5d04cf15
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-03 13:55:48 +0530

    configured EC2

--- main.tf
+++ main.tf
@@ -70,3 +70,20 @@ resource "aws_key_pair" "mtc_auth"{
     key_name = "mtckey"
     public_key = file("~/.ssh/mtckey.pub")
 }
+
+resource "aws_instance" "dev_node"{
+    instance_type = "t2.micro"
+    ami           = data.aws_ami.server_ami.id
+
+    tags = {
+        name = "dev-node"
+    }
+
+    key_name = aws_key_pair.mtc_auth.id
+    vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+    subnet_id = aws_subnet.mtc_public_subnet.id
+
+    root_block_device {
+        volume_size = 10
+    }
+}
