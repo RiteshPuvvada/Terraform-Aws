e74fcecbdb6978e861a13eafb7abb3c17846f25d
RiteshPuvvada <riteshpuvvada@gmail.com>
2022-06-03 14:16:35 +0530

    adding userdata to bootstrap instance and install docker

--- main.tf
+++ main.tf
@@ -73,17 +73,17 @@ resource "aws_key_pair" "mtc_auth"{
 
 resource "aws_instance" "dev_node"{
     instance_type = "t2.micro"
-    ami           = data.aws_ami.server_ami.id
-
-    tags = {
-        name = "dev-node"
-    }
-
+    ami = data.aws_ami.server_ami.id
     key_name = aws_key_pair.mtc_auth.id
     vpc_security_group_ids = [aws_security_group.mtc_sg.id]
     subnet_id = aws_subnet.mtc_public_subnet.id
+    user_data = file("userdata.tpl")
 
     root_block_device {
         volume_size = 10
+    }
+
+    tags = {
+        name = "dev-node"
     }
 }
